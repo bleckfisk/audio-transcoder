19a1f10441551970fa4b0c9b5a1e7b5d9dcca9bc
Oskar <Brorsson.oskar@gmail.com>
2020-01-22 14:55:55 +0100

    adjust operators in tests

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -8,12 +8,12 @@ from service.validators import (
 
 def test_check_type_with_wav():
     correct_input = "wav"
-    assert check_type(correct_input) == True
+    assert check_type(correct_input) is True
 
 
 def test_check_type_not_wav():
     not_wav = 'mp3'
-    assert check_type(not_wav) == False
+    assert check_type(not_wav) is False
 
 
 def test_check_input_structure_correct_input():
@@ -21,7 +21,7 @@ def test_check_input_structure_correct_input():
         "file": "string",
         "type": "string"
     }
-    assert check_input_structure(correct_input) == True
+    assert check_input_structure(correct_input) is True
 
 
 def test_check_input_structure_bad_input():
@@ -38,15 +38,16 @@ def test_check_input_structure_bad_input():
         }
     }
 
-    assert check_input_structure(bad_input) == False
-    assert check_input_structure(bad_input_2) == False
+    assert check_input_structure(bad_input) is False
+    assert check_input_structure(bad_input_2) is False
+
 
 def test_check_output_structure_correct_output():
     correct_output = {
         "file": "string",
         "type": "string"
     }
-    assert check_output_structure(correct_output) == True
+    assert check_output_structure(correct_output) is True
 
 
 def test_check_output_structure_bad_output():
@@ -63,8 +64,8 @@ def test_check_output_structure_bad_output():
         }
     }
 
-    assert check_input_structure(bad_output) == False
-    assert check_input_structure(bad_output_2) == False
+    assert check_input_structure(bad_output) is False
+    assert check_input_structure(bad_output_2) is False
 
 
 def test_check_message_structure():
@@ -113,8 +114,6 @@ def test_check_message_structure():
         }
     }
 
-    assert check_message_structure(correct_structure) == True
-    assert check_message_structure(bad_structure) == False
-    assert check_message_structure(bad_structure_2) == False
-
-
+    assert check_message_structure(correct_structure) is True
+    assert check_message_structure(bad_structure) is False
+    assert check_message_structure(bad_structure_2) is False
