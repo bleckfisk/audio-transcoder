03b2be21b449367e46be254ee746217a932b89ea
Oskar <Brorsson.oskar@gmail.com>
2020-01-29 10:08:41 +0100

    add test for download()

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,16 +4,17 @@ from uuid import uuid4
 
 
 @pytest.fixture
-def fixture_s3_bucket():
+def s3_bucket():
     from service.aws_boto3 import create_s3_resource
     resource = create_s3_resource()
 
     bucket_name = str(uuid4())
     try:
-        bucket = resource.meta.client.create_bucket(Bucket=bucket_name)
+        bucket = resource.create_bucket(Bucket=bucket_name)
     except ClientError:
         bucket = resource.Bucket(bucket_name)
         bucket.objects.all().delete()
+    print(bucket)
     yield bucket
     bucket.objects.all().delete()
     bucket.delete()
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,9 +1,25 @@
-def test_download(fixture_s3_bucket):
-    # 
+from service.core import upload, download
+from uuid import uuid4
+from service.aws_boto3 import create_s3_resource
 
 
-def test_upload(fixture_s3_bucket):
+def test_download(s3_bucket):
 
-    # set up file to upload with data that are strings
+    """
+    Tests proves that subject returned from download() contains the
+    same data that we uploaded manually.
+    """
 
-    # upload with put_obj
+    key = str(uuid4())
+    body = b"testdata"
+    bucket = s3_bucket.name
+    s3_bucket.put_object(Key=key, Body=body)
+
+    payload = {
+        "key": key,
+        "bucket": bucket
+    }
+
+    subject = download(create_s3_resource(), payload)
+
+    assert subject.read() == body
