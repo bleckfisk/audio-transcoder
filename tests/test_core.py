58d11c3bbfa45a65442186215d32aa77aba1be96
Oskar <Brorsson.oskar@gmail.com>
2020-01-29 10:48:47 +0100

    add test for upload and upload/download data assertion

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,3 +1,4 @@
+import io
 from service.core import upload, download
 from uuid import uuid4
 from service.aws_boto3 import create_s3_resource
@@ -23,3 +24,55 @@ def test_download(s3_bucket):
     subject = download(create_s3_resource(), payload)
 
     assert subject.read() == body
+
+
+def test_upload(s3_bucket):
+    """
+    Test proves that file created here and sent to
+    upload() is uploaded accordingly.
+    """
+
+    key = str(uuid4())
+    bucket = s3_bucket.name
+
+    payload = {
+        "key": key,
+        "bucket": bucket
+    }
+
+    some_data = b"test123"
+    file_to_upload = io.BytesIO(some_data)
+
+    upload(create_s3_resource(), file_to_upload, payload)
+
+    my_bucket_objects = s3_bucket.objects.all()
+
+    for obj in my_bucket_objects:
+        assert obj.key == key
+
+
+def test_upload_download_data_assertion(s3_bucket):
+
+    key = str(uuid4())
+    bucket = s3_bucket.name
+
+    payload = {
+        "key": key,
+        "bucket": bucket
+    }
+
+    some_data = b"test123"
+    file_to_upload = io.BytesIO()
+    file_to_upload.write(some_data)
+
+    upload(create_s3_resource(), file_to_upload, payload)
+
+    my_bucket_objects = s3_bucket.objects.all()
+
+    for obj in my_bucket_objects:
+        assert obj.key == key
+
+    downloaded = download(create_s3_resource(), payload)
+
+    assert downloaded.read() == file_to_upload.read()
+
