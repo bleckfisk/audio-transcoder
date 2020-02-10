bf16f29452f3a04a5bbeeb4bf2c0d7f1bb36d4a8
Oskar <Brorsson.oskar@gmail.com>
2020-02-10 09:30:44 +0100

    remove old transcoder tests due to new ones

--- tests/test_transcoder.py
+++ tests/test_transcoder.py
@@ -1,44 +1,6 @@
 import pytest
-import io
 import os
 from service.transcoder import transcode
-from pydub.audio_segment import CouldntDecodeError
-
-
-def test_transcode_unsupported_data(unsupported_transcode_data):
-    """
-    Test that proves that exceptions are raised when files with bad formats
-    are passed to the transcoder. In this case we have the following data:
-
-    handles[0] == jpg
-    handles[1] == pdf
-
-    """
-    handles = unsupported_transcode_data["handles"]
-    outputs = unsupported_transcode_data["outputs"]
-
-    with pytest.raises(IndexError):
-        file = io.BytesIO(handles[0].read())
-        for output in outputs:
-            transcode(file, output)
-
-    with pytest.raises(CouldntDecodeError):
-        file = io.BytesIO(handles[1].read())
-        for output in outputs:
-            transcode(file, output)
-
-
-def test_transcode_supported_data(supported_transcode_data):
-    """
-    Test that in contrast to above does not raise exception as
-    the format is supported.
-
-    supported_transcode_data["handles"][0] = wav
-
-    """
-    file = supported_transcode_data["handles"][0]
-    output = supported_transcode_data["outputs"]
-    transcode(file, output)
 
 
 def test_all_supported_testfiles(list_of_supported_files):
