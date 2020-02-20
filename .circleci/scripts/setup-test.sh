e09b06ed8a39269cbb681b3f12d2991e10848f4e
Oskar <Brorsson.oskar@gmail.com>
2020-02-20 09:45:57 +0100

    delete accidental merge from amuseio

--- .circleci/scripts/setup-test.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-sudo apt-get update && apt-get -y install ffmpeg libavcodec-extra
-sudo pip install --upgrade pip && sudo pip install pipenv awscli boto3
-sudo pipenv install --system --deploy --dev
